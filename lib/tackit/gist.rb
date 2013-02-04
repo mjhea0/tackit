211affd1b55e608e115c290f7970f1d04d3c3ae2
Adrian Moses <ammoses89@gmail.com>
2013-02-04 13:04:56 -0800

    ok fixed

--- lib/tackit/gist.rb
+++ lib/tackit/gist.rb
@@ -39,8 +39,9 @@ module Tackit
     end
     
     def self.create_gist(token, files)
-      uri = URI("https://api.github.com/gists?access_token=#{token}")
+      uri = URI("https://api.github.com/gists")
       request = Net::HTTP::Post.new(uri.path)
+      request.add_field "Authorization", "token #{token}"
       params = {
         "description" => '',
         "public" => true,
@@ -50,7 +51,7 @@ module Tackit
       }
       files.each do |f|
         file = File.open(f, 'r')
-        params["files"][f] = { "content" => file.read }
+        params["files"][File.basename(f)] = { "content" => file.read }
       end
         
       request.body = params.to_json
@@ -59,8 +60,15 @@ module Tackit
       end
       
       res_hash = JSON.parse(response.body)
-      puts res_hash
-      
+      if(res_hash.length == 1 && res_hash['message'])
+        puts "Token expired. Remove yaml file"
+      else
+        res_hash.each do |gist|
+          puts "\e[36m Desc: #{gist['desctiption']}\e[m"
+          puts "\e[34m URL: #{gist['html_url']}\e[m"
+        end
+        puts "Gist successfully created"
+      end 
     end
     
     def self.get_gist(token)
